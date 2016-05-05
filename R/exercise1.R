624ee451610f83a26e55b342b0ecd9adbae82dd7
OssiB <ossi.bister@gmail.com>
2016-05-05 18:05:00 +0300

    modified

--- R/exercise1.R
+++ R/exercise1.R
@@ -89,6 +89,7 @@ for (w in weights){
   errors_wma[index] <- MAPE(timeseries_data,weighted_ma(timeseries_data,c(w,1-w)));
   index <- index +1;
 }
+
 index <- 1 
 alphas <- seq(0,1,by = 0.01)
 errors_smooth <- rep(0,length(alphas))
@@ -96,8 +97,12 @@ for (alpha  in alphas) {
   errors_smooth[index] <- MAPE(timeseries_data,exp_smoothing(timeseries_data,alpha));
   index <- index +1;
 }
-
-
+errors["naive"] <- MAPE(timeseries_data,naiv(timeseries_data))
+errors["ma"] <- MAPE(timeseries_data,ma(timeseries_data,2))
+errors["wma"] <- errors_wma[which(errors_wma == min(errors_wma))]
+errors["expsm"] <- errors_smooth[which(errors_smooth == min(errors_smooth))]
+errors[which(errors == min(errors))]
+# So the exponential smoothing shows the best performance
 
 
 
